81973605f4317a3c1efbdaed9fba8b83b56b9213
123jaffar <116568862+123jaffar@users.noreply.github.com>
2023-04-10 22:20:46 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+var numbers=[1,2,3,4,5,6,7,8,9,10,12,13,16]
+ var sum=0
+function sumOfEvenNumbers(numbers){
+ for(var I=0; I<numbers.length; I++)
+    if(numbers[i]%2==0)
+    sum+=numbers[i]
+ return sum
+}
+
+Q-No-4
+var are=[10,14,24,30,28,46,50,54,60,70]
+
+  var sum=0
+var avg=0
+ function average(are){
+  for(var I=0; I<are.length; I++){
+  sum+=are[i]
+ avg=sum/(are.length-1)
+}
+return avg
+}
+
+
+Q- No-8
+
+  var prodNum=[2,3,5,7,8,9,12]
+var x=1
+
+
+ function product(producNum){
+  for(var I=0; I<prodNum.length; I++){
+ x*=prodNum[I]
+}
+return x
+}
+
+
+ 
