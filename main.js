3f8431d3bd42db3473214e51a05902f9a726d9e2
123jaffar <116568862+123jaffar@users.noreply.github.com>
2023-04-11 18:39:32 +0500

    Update main.js

--- main.js
+++ main.js
@@ -20,6 +20,38 @@ return count
 }
 console.log(count)
 
+Q-No-3
+var strarr=["Jaffar", "Rahib", " Sulaiman", 
+ "Aziz", " Bilal"]
+ let l=strarr.length
+
+ function longeststr(strarr){
+  var longest=strarr[0]
+
+  for(var I=0; I<strarr.l; I++){
+     if(strarr[I]>longest){
+   longest=strarr[I]
+  }
+}
+console.log(longest)
+}
+ 
+
+  Q-No-5
+ var student={
+ name: "Amjad",
+ age: 15,
+ class:" 9th"
+}
+ function getkey(student){
+  console.log((object.key(student))
+ }
+
+
+
+
+
+
 
 Q-No-4
 var are=[10,14,24,30,28,46,50,54,60,70]
@@ -33,6 +65,32 @@ var avg=0
  avg=sum/(are.length-1)
 
 return avg
+}
+console.log(avg)
+
+Q-No-6
+ var patients=[{"name:"arshad", age:40, tmp:37
+},{name:"ayub", age:29, tmp: 36},{
+ name:"asghar", age:50, tmp:35}]
+  function getkeys(patients){
+  for(var I=0; I<patients.length: I++)
+{
+  console.log(object.key(patients[I]))
+}
+}
+
+
+
+
+
+Q-No-7
+ var myarr=[{"apple", "banana", "melon",
+},{"apple"," tomato","potato"},{"onion","lemon","
+ "strawberry"}
+]
+ function get unique(myarr){
+  for(var i=0; I<myarr.length; I++){
+  }
 }
 
 
@@ -48,6 +106,8 @@ var x=1
 }
 return x
 }
+console.log(x)
+
 
 
 
